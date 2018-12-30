4c043e7fe03cc4a25a017e2aa2cb80c77dbe7270
Freddy Ferrao <freddy.ferrao@alight.com>
2018-12-29 20:47:04 -0800

    Added Foursquare Venue Ids to markers

--- js/markers.js
+++ js/markers.js
@@ -4,6 +4,7 @@ var properties = [ {
     lng: -122.3281032,
     address: "1124 Pike St, Seattle, WA 98101",
     type: "Coffee Bar",
+    fsid: "4b670e59f964a52066372be3",
     icon: "../content/img/marker.png"
 }, {
     name: "Pie Bar",
@@ -11,6 +12,7 @@ var properties = [ {
     lng: -122.327419,
     address: "1361 E Olive Way, Seattle, WA 98122",
     type: "Bar",
+    fsid: "517c94dee4b0f7d84c4424a3",
     icon: "../content/img/marker.png"
 }, {
     name: "Sugar Hill",
@@ -18,6 +20,7 @@ var properties = [ {
     lng: -122.325992,
     address: "414 E Pine St, Seattle, WA 98122",
     type: "Restaurant",
+    fsid: "585242cda913303f63f584be",
     icon: "../content/img/marker.png"
 }, {
     name: "General Porpoise Doughnuts",
@@ -25,6 +28,7 @@ var properties = [ {
     lng: -122.318738,
     address: "1020 E Union St, Seattle, WA 98122",
     type: "Donuts",
+    fsid: "56380a1ecd10193ef6e851ce",
     icon: "../content/img/marker.png"
 }, {
     name: "Manao Thai Street Eats",
@@ -32,6 +36,7 @@ var properties = [ {
     lng: -122.3157616,
     address: "1222 E Pine St, Seattle, WA 98122",
     type: "Restaurant",
+    fsid: "542115e5498e32acece95523",
     icon: "../content/img/marker.png"
 }, {
     name: "Unicorn",
@@ -39,5 +44,6 @@ var properties = [ {
     lng: -122.317241,
     address: "1118 E Pike St, Seattle, WA 98122",
     type: "Bar",
+    fsid: "4ad4e8faf964a5209ffc20e3",
     icon: "../content/img/marker.png"
 } ];
